6539ef66338cfde4055af06fea6a55a625e347e5
Paulo Junio <pjrrodrigues@sga.pucminas.br>
2019-05-01 20:04:19 -0300

    Aprendendo como que faz inserção de valores

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,55 @@ import axios from 'axios';
 
 class App extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      nomMaterial: ['io'],
+      quaMaterial: ['12']
+    };
+
+    this.addItem = this.addItem.bind(this);
+    console.log(this.state)
+  }
+
+  addItem() {
+   
+    var nomMaterial = [...this.state.nomMaterial];
+    var quaMaterial = [...this.state.quaMaterial];
+    nomMaterial.push(this.newNomeMaterial.value);
+    quaMaterial.push(this.newQuantidadeMaterial.value);
+    this.setState({nomMaterial});
+    this.setState({quaMaterial});
+    console.log(this.state.nomMaterial);
+    console.log(this.state.quaMaterial);
+  }
   render() {
     return (
       <React.Fragment>
-        <div classname="containerundo" id="fundo">
+        <div className="containerFundo" id="fundo">
         <div className="container">
           <h1>
             Novo Material
           </h1>
         </div>
         <div className="containerNew">
-          <form action="post">
+          
               <h3>Nome do material</h3>
-              <input type="text" name="nome_material" placeholder="Exemplo: linha" className="form-control" id="form-login"/>
+              <input ref={input => this.newNomeMaterial = input} type="text" name="nome_material" placeholder="Exemplo: linha" className="form-control" id="form-login"/>
               <h3>Quantidade desse material</h3>
-              <input type="number" name="quantidade_material" placeholder="Somente números" className="form-control" id="form-login"/>
-          </form>
+              <input ref={input => this.newQuantidadeMaterial = input} type="number" name="quantidade_material" placeholder="Somente números" className="form-control" id="form-login"/>
+              <div className="container-ask">
+                <button onClick={this.addItem} name="adicionar_material" className="btn btn-sign-up center-block pull-left">Adicionar material</button>
+                <button type="submit" name="proximo_passo" className="btn btn-sign-up center-block pull-right">Proximo passo</button>
+              </div>
+              <div>
+              {this.state.nomMaterial.map(function(d, idx){
+                  return (<li key={idx}>{d}</li>)
+              })}
+              </div>
         </div>
       </div>
-      <div className="container-ask">
-        <button type="submit" name="adicionar_material" className="btn btn-sign-up center-block pull-left">Adicionar material</button>
-        <button type="submit" name="proximo_passo" className="btn btn-sign-up center-block pull-right">Proximo passo</button>
-      </div> 
+       
     </React.Fragment>
     );
   }
