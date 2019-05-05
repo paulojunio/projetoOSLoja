954dca711beb77dc16ce517aae81e988e868aca2
GiovannaR <giovannariqueti@gmail.com>
2019-05-05 15:27:58 -0300

    Path

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/api/', (req, res) => {
-  res.send({ express: 'Teste' });
-});
+//app.get('/api/', (req, res) => {
+//  res.send({ express: 'Teste' });
+//});
 
 
 app.post('/api/otimizar', (req, res) => {
     GlpkSolver.solve(req.body, res);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
