5077a964d8449b32fe1e021c6725cc884d06bb6a
GiovannaR <giovannariqueti@gmail.com>
2019-05-05 15:58:00 -0300

    Isso

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const GlpkSolver = require('./GLPK');
+var path = require('path');
 
 const app = express();
-//app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
